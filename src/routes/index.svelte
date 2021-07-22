0c972686ec9e32955aa3ef78c23e84570030d993
Max Richter <max.richter@smail.th-koeln.de>
2021-07-22 13:10:16 +0200

    feat: add content to index

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,49 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<header>
+	<ul>
+		<li>
+			<a href="Stuff"> Makes</a>
+		</li>
+	</ul>
+</header>
+<main>
+	<!-- Intro Section -->
+	<section id="intro">
+		<h1>
+			<b>Soziale Teilhabe</b><br />
+			mit einem Klick.
+		</h1>
+	</section>
+
+	<!-- Explainer Section -->
+	<section id="explainer">
+		<h3>LunchtimeBox</h3>
+
+		<p>
+			Die Lunchtimebox ermöglicht Teilnahme am sozialen Leben mit nur einem Klick Lorem ipsum dolor
+			sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet
+			Lorem ipsum dolor sit amet Lorem ipsm dfgodfngdfg sdf gsdfg sdf gsdf gsd fgsdfgsfvcb sdfbs
+			dfvsdfv sdfs dfbsdfbsdf bsdfb sfgb sfgb sfgb sfgbs fbsf gbsfgb sfgb sdfgb sdfb sdfgb sdfb sdfb
+			sdfb sdfb sdb sd bsdf bsd fb sdfb sdfb sfdgb sdfg bsdf bsd
+		</p>
+	</section>
+
+	<section id="roadmap">
+		<h2>Stuff and things</h2>
+	</section>
+
+	<section id="about-us">
+		<p>
+			sind ein interdisziplinäres Team, mit Fähigkeiten im Bereich Hard- und Software, UI / UX
+			Design, Entrepeneurship und dem gemeinsamen Wunsch diese Fähigkeiten in einem sozialen Kontext
+			einzusetzen.
+		</p>
+	</section>
+
+	<section id="team">
+		<Person name="Majid" description="" img="asdasd" />
+	</section>
+</main>
+
+<style lang="scss">
+	@import '../styles/index.scss';
+</style>
