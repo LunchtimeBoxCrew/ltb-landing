7440e56f5cecbefd1f4dafa582d6d89dc8649b80
Naskichan <gitlab@naski.org>
2021-07-26 13:05:44 +0200

    coloring, formatting

--- src/lib/Person.svelte
+++ src/lib/Person.svelte
@@ -5,45 +5,47 @@
     export let img:string;
 
 </script>
-
-
 <div id='wrapper'>
-    
     <img src={img} />
-    <div id='infobox' class='ltbgreen'>
-        <h2 class=''>
+    <div id='infobox' class='ltbgreen white'>
+        <h2 class='xl'>
             {name}
         </h2>
         <p>
             {description}
         </p>
     </div>
-
-
-
 </div>
-
 <style>
+    
     #wrapper {
-        height: 12rem;
-        width: 36rem;
-        margin: 2rem;
+        height: 16rem;
+        width: 32rem;
+        margin: 1rem;
         display: flex;
         flex-direction: row;
     }
     #infobox {
-        width: 66%;
+        width: 50%;
+        padding: 1rem;
+        border-radius: 0rem 1rem 1rem 0rem;
     }
     img {
         height: 100%;
         width: 33%;
         overflow-y: hidden;
         object-fit: cover;
-        object-position: 20% 20%;
+        object-position: 50% 50%;
     }
 
     div.ltbgreen {
         background: #00877C;
+    }
+    h2.xl {
+        font-size: 2rem;
+    }
+    .white {
+        color: white;
     }
 
 </style>
