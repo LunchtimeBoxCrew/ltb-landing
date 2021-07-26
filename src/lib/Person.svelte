3640c1dfc37120c8e2be94ff60043e3135a4fca2
Naskichan <gitlab@naski.org>
2021-07-26 13:28:19 +0200

    person mobile scaling

--- src/lib/Person.svelte
+++ src/lib/Person.svelte
@@ -17,29 +17,41 @@
     </div>
 </div>
 <style>
-    
+
     #wrapper {
-        height: 16rem;
-        width: 32rem;
-        margin: 1rem;
+        height: 12rem;
+        width: 100%;
         display: flex;
         flex-direction: row;
-        border: 1rem 0 0 1rem;
-    }
-    #infobox {
-        width: 66%;
-        padding: 1rem;
-        border-radius: 0rem 1rem 1rem 0rem;
+        margin-bottom: 1rem;
     }
     img {
-        border-radius: 1rem 0 0 1rem;
         display: block;
         height: 100%;
         width: 33%;
 
         overflow-y: hidden;
         object-fit: cover;
-        object-position: 50% 50%;
+    }
+    #infobox {
+            width: 66%;
+            padding: 1rem;
+        }
+    @media(min-width: 580px) {
+        
+        #wrapper {
+            height: 16rem;
+            width: 32rem;
+            margin: 1rem;
+            border: 1rem 0 0 1rem;
+            object-position: 50% 50%;
+        }
+        #infobox {
+            border-radius: 0rem 1rem 1rem 0rem;
+        }
+        img {
+            border-radius: 1rem 0 0 1rem;
+        }
     }
     
     /*
