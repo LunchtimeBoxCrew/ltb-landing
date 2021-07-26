a87e6e36ca32d282d3b4a8b36f8d9034a690ec35
Naskichan <gitlab@naski.org>
2021-07-26 14:54:41 +0200

    basic Roadmap element design

---
+++ src/lib/RoadMapEntry.svelte
@@ -0,0 +1,42 @@
+<script lang='ts'>
+    export let position:string;
+    export let name:string;
+    export let description:string;
+
+    console.log(position)
+
+</script>
+<div id='wrapper'>
+    {#if position == 'left'}
+    <div class='left element ltbgreen'>
+        {name}
+        {description}
+    </div>
+    {:else}
+    <div class='right element ltbgreen'>
+        {name}
+        {description}
+    </div>
+    {/if}
+</div>
+
+<style>
+    #wrapper {
+        width: 100%;
+        padding: 5rem;
+    }
+    .left {
+
+
+    }
+    .right {
+        float: right;
+    }
+    .element {
+        width: 12rem;
+        padding: 3rem;
+    }
+    .ltbgreen {
+        background: #00877C;
+    }
+</style>
