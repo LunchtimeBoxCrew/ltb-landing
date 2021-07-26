981fc97d9d298377fd286945b2b471e745af444d
Naskichan <gitlab@naski.org>
2021-07-26 13:18:49 +0200

    image radius, clearup

--- src/lib/Person.svelte
+++ src/lib/Person.svelte
@@ -11,7 +11,7 @@
         <h2 class='xl'>
             {name}
         </h2>
-        <p>
+        <p class='m'>
             {description}
         </p>
     </div>
@@ -24,25 +24,35 @@
         margin: 1rem;
         display: flex;
         flex-direction: row;
+        border: 1rem 0 0 1rem;
     }
     #infobox {
-        width: 50%;
+        width: 66%;
         padding: 1rem;
         border-radius: 0rem 1rem 1rem 0rem;
     }
     img {
+        border-radius: 1rem 0 0 1rem;
+        display: block;
         height: 100%;
         width: 33%;
+
         overflow-y: hidden;
         object-fit: cover;
         object-position: 50% 50%;
     }
-
+    
+    /*
+        Definitions
+    */
     div.ltbgreen {
         background: #00877C;
     }
-    h2.xl {
+    .xl {
         font-size: 2rem;
+    }
+    .m {
+        font-size: 1rem;
     }
     .white {
         color: white;
