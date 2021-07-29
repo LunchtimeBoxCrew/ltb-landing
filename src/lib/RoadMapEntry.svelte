15398dfcd93b9e3dce44b85cee35805e48702339
Naskichan <gitlab@naski.org>
2021-07-29 13:11:16 +0200

    starting rework of RoadMapEntries

--- src/lib/RoadMapEntry.svelte
+++ src/lib/RoadMapEntry.svelte
@@ -1,42 +1,18 @@
 <script lang='ts'>
-    export let position:string;
     export let name:string;
+    export let date:string;
     export let description:string;
-
-    console.log(position)
 
 </script>
 <div id='wrapper'>
-    {#if position == 'left'}
-    <div class='left element ltbgreen'>
-        {name}
-        {description}
-    </div>
-    {:else}
-    <div class='right element ltbgreen'>
-        {name}
-        {description}
-    </div>
-    {/if}
+    <h2 class='gray'>{name}</h2>
+    <p>{date}</p>
+    <p>{description}</p>
+    
 </div>
 
 <style>
-    #wrapper {
-        width: 100%;
-        padding: 5rem;
-    }
-    .left {
-
-
-    }
-    .right {
-        float: right;
-    }
-    .element {
-        width: 12rem;
-        padding: 3rem;
-    }
-    .ltbgreen {
-        background: #00877C;
+    .gray {
+        background-color: #f8f8f8;
     }
 </style>
