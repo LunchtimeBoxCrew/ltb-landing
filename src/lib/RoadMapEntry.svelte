8b0b67979b7e32708ff8453a7e2bffcde8cad24c
Naskichan <gitlab@naski.org>
2021-07-29 17:27:56 +0200

    trying out new image based layout

--- src/lib/RoadMapEntry.svelte
+++ src/lib/RoadMapEntry.svelte
@@ -1,17 +1,39 @@
 <script lang='ts'>
+    import { slide } from 'svelte/transition';
     export let name:string;
     export let date:string;
     export let description:string;
+    export let image:string;
+
+    let showDesc = false;
+    function switchDesc() {
+        console.log('desc was '+showDesc);
+        showDesc ? showDesc = false : showDesc = true;
+    }
 
 </script>
-<div id='wrapper'>
-    <h2 class='gray'>{name}</h2>
+<div id='wrapper' class='gray' on:click={switchDesc} style='--bg-image = {image}'>
+    <h2>{name}</h2>
     <p>{date}</p>
-    <p>{description}</p>
-    
+    {#if showDesc}
+    <div transition:slide>
+        {description}
+    </div>
+    {/if}
 </div>
 
 <style>
+    #wrapper {
+        background-image: url(--bg-image);
+        width: 50%;
+        border-radius: 1rem;
+        padding: 1rem;
+        margin: 1rem;
+        transition: background-color .5s ease-in-out;
+    }
+    #wrapper:hover {
+            background-color: darkgray;
+        }
     .gray {
         background-color: #f8f8f8;
     }
