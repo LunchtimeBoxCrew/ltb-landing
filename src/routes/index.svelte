1fa1cef6f768977765d8da7940f0d7261dbfd870
Max Richter <max.richter@smail.th-koeln.de>
2021-08-03 13:46:07 +0200

    feat: compress images with vite-imagetool

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,6 +1,17 @@
 <script lang="ts">
+	import PortraitDario from '$lib/images/people/dario.jpeg?w=300;400;500&format=webp&srcset';
+	import PortraitFabian from '$lib/images/people/fabian.jpeg?w=300;400;500&format=webp&srcset';
+	import PortraitLara from '$lib/images/people/lara.jpeg?w=300;400;500&format=webp&srcset';
+	import PortraitMajid from '$lib/images/people/majid.jpeg?w=300;400;500&format=webp&srcset';
+	import PortraitMax from '$lib/images/people/max.jpeg?w=300;400;500&format=webp&srcset';
+	import PortraitJonas from '$lib/images/people/naski.jpeg?w=300;400;500&format=webp&srcset';
+	import PortraitTimo from '$lib/images/people/timo.jpeg?w=300;400;500&format=webp&srcset';
+	import StockAcceleration from '$lib/images/stock/acceleration.jpg?w=300;400;500&format=webp&srcset';
+	import StockIdeation from '$lib/images/stock/ideation.jpg?w=300;400;500&format=webp&srcset';
+	import StockProject from '$lib/images/stock/project.jpg?w=300;400;500&format=webp&srcset';
 	import Person from '$lib/Person.svelte';
 	import RoadMapEntry from '$lib/RoadMapEntry.svelte';
+	import HeroHeader from './HeroHeader.jpg?w=300;400;1000&format=webp&flatten=true&srcset';
 </script>
 
 <header>
@@ -11,20 +22,23 @@
 <main>
 	<!-- Intro Section -->
 	<section id="intro">
-		<div id ='heading' class='white'>
+		<img srcset={HeroHeader} alt="smiling man sitting infront of desk with tablet" />
+		<div id="heading" class="white">
 			<h1>
 				<b>Soziale Teilhabe</b><br />
 				mit einem Klick<b>.</b>
 			</h1>
 		</div>
 		<div id="footer">
-			<div id='subtextwrapper'>
-				<p class='white'>Wir geben Menschen ein einfaches Werkzeug gegen die soziale Isolation an die Hand.</p>
+			<div id="subtextwrapper">
+				<p class="white">
+					Wir geben Menschen ein einfaches Werkzeug gegen die soziale Isolation an die Hand.
+				</p>
 			</div>
 			<button id="contact">
 				<div id="content">
-					<p> Kontaktiere uns </p>
-					<img src='images/email.svg' />
+					<p>Kontaktiere uns</p>
+					<img alt="email icon" src="images/email.svg" />
 				</div>
 			</button>
 		</div>
@@ -33,7 +47,7 @@
 	<!-- Explainer Section -->
 	<section id="explainer" class="dark">
 		<h2>LunchtimeBox</h2>
-		<p id='text'>
+		<p id="text">
 			Die Lunchtimebox ermöglicht Teilnahme am sozialen Leben mit nur einem Klick Lorem ipsum dolor
 			sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet
 			Lorem ipsum dolor sit amet Lorem ipsm dfgodfngdfg sdf gsdfg sdf gsdf gsd fgsdfgsfvcb sdfbs
@@ -44,10 +58,27 @@
 
 	<section id="roadmap">
 		<h2>Roadmap - Where we've been, where we're going</h2>
-		<RoadMapEntry name='Entstehung' date='2019' image='images/ideation.jpg' description='Im Kurs Design Thinking mit Ivonne Preußer stellte sich die Frage "Was brauchen Nutzer von Essen auf Rädern Services wirklich. Diesem Ansatz folgend entwickelten wir die Idee der Lunchtimebox mit einem ersten Prototypen' />
-		<RoadMapEntry name='Project Make 1' date='2020' image='images/project.jpg' description='Im Project Make 1 bekamen wir als Team nun die Möglichkeit, an der Idee der LunchtimeBox zu arbeiten und einen ersten Protoypen zu erschaffen' />
-		<RoadMapEntry name='Kickstart' date='2021' image='images/acceleration.jpg' description='Im Verlauf des Project Make 1 konnten wir das Team des Kickstart@TH Köln mit unserer Idee überzeugen und eine Förderung von 7.500€ für die Weiterentwicklung unseres Prototypen erhalten.' />
-		
+		<RoadMapEntry
+			name="Entstehung"
+			date="2019"
+			description="Im Kurs Design Thinking mit Ivonne Preußer stellte sich die Frage &quot; Was brauchen Nutzer von Essen auf Rädern Services wirklich. Diesem Ansatz folgend entwickelten wir die Idee der Lunchtimebox mit einem ersten Prototypen"
+		>
+			<img srcset={StockIdeation} alt="Ideation" />
+		</RoadMapEntry>
+		<RoadMapEntry
+			name="Project Make 1"
+			date="2020"
+			description="Im Project Make 1 bekamen wir als Team nun die Möglichkeit, an der Idee der LunchtimeBox zu arbeiten und einen ersten Protoypen zu erschaffen"
+		>
+			<img srcset={StockProject} alt="Project Make 1" />
+		</RoadMapEntry>
+		<RoadMapEntry
+			name="Kickstart"
+			date="2021"
+			description="Im Verlauf des Project Make 1 konnten wir das Team des Kickstart@TH Köln mit unserer Idee überzeugen und eine Förderung von 7.500€ für die Weiterentwicklung unseres Prototypen erhalten."
+		>
+			<img srcset={StockAcceleration} alt="Kickstart" />
+		</RoadMapEntry>
 	</section>
 
 	<section id="about-us">
@@ -60,13 +91,27 @@
 	</section>
 
 	<section id="team">
-		<Person name="Majid," description="packt an, zieht durch" img="images/majid.jpeg" />
-		<Person name="Timo," description="der perfektionistische Informatiker" img="images/timo.jpeg" />
-		<Person name="Lara," description="unsere kreative Powereinheit" img="images/lara.jpeg" />
-		<Person name="Dario," description="unsere Emotionale Intelligenz" img="images/dario.jpeg" />
-		<Person name="Jonas," description="der immer noch zwei Schritte weiterdenkt" img="images/naski.jpeg" />
-		<Person name="Fabian," description="&quot;MacMakeThings&quot;" img="images/fabian.jpeg" />
-		<Person name="Max," description="unser kreativer Programmierer" img="images/max.jpeg" />
+		<Person name="Majid" description="packt an, zieht durch">
+			<img srcset={PortraitMajid} type="image/webp" alt="person" />
+		</Person>
+		<Person name="Timo" description="der perfektionistische Informatiker">
+			<img srcset={PortraitTimo} type="image/webp" alt="person" />
+		</Person>
+		<Person name="Lara" description="unsere kreative Powereinheit">
+			<img srcset={PortraitLara} type="image/webp" alt="person" />
+		</Person>
+		<Person name="Dario" description="unsere Emotionale Intelligenz">
+			<img srcset={PortraitDario} type="image/webp" alt="person" />
+		</Person>
+		<Person name="Jonas" description="der immer noch zwei Schritte weiterdenkt">
+			<img srcset={PortraitJonas} type="image/webp" alt="person" />
+		</Person>
+		<Person name="Fabian" description="&quot;MacMakeThings&quot;">
+			<img srcset={PortraitFabian} type="image/webp" alt="person" />
+		</Person>
+		<Person name="Max" description="unser kreativer Programmierer">
+			<img srcset={PortraitMax} type="image/webp" alt="person" />
+		</Person>
 	</section>
 </main>
 
@@ -83,7 +128,6 @@
 		float: right;
 		padding: 2rem;
 		> a {
-			
 			text-decoration: none;
 			font-weight: 200;
 			color: white;
@@ -96,15 +140,20 @@
 	}
 
 	#intro {
-		background-image: url('./HeroHeader.jpg');
-		background-size: cover;
+		position: relative;
+		> img {
+			position: absolute;
+			z-index: -1;
+			width: 100%;
+			height: 100%;
+			object-fit: cover;
+		}
 
-		 h1 {
+		h1 {
 			font-size: 8rem;
 			font-weight: 300;
 			line-height: 8rem;
 			margin: 0;
-			
 		}
 		#heading {
 			padding: 5rem;
@@ -122,7 +171,7 @@
 			font-size: 1.4rem;
 			width: 48rem;
 		}
-		#contact {
+		> #footer > #contact {
 			all: unset;
 			background-color: white;
 			width: 16rem;
@@ -135,12 +184,7 @@
 				flex-direction: row;
 				justify-content: space-around;
 			}
-			svg {
-				fill: gray;
-			}
 		}
-		
-		 
 	}
 	#explainer {
 		padding: 5rem;
@@ -154,7 +198,6 @@
 			width: 60%;
 			color: darkgray;
 		}
-
 	}
 
 	#roadmap {
@@ -163,7 +206,7 @@
 			font-weight: 700;
 		}
 	}
-	
+
 	#team {
 		display: flex;
 		flex-direction: row;
@@ -181,5 +224,4 @@
 	#about-us {
 		margin: 5rem;
 	}
-	
 </style>
