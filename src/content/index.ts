083bbd8b565aca77b55118e93685e13c74830bc2
Naskichan <gitlab@naski.org>
2021-08-13 15:42:25 +0200

    added further content

--- src/content/index.ts
+++ src/content/index.ts
@@ -1,3 +1,4 @@
 export { html as intro } from './intro.md';
 export { html as idea } from './idea.md';
 export { html as formation } from './formation.md';
+export { html as firstprototype } from './firstprototype.md';
