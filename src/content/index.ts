1f602b23b219ad2e495a4038e047f171020a78ec
Naskichan <gitlab@naski.org>
2021-08-13 15:33:51 +0200

    fixing merge error

--- src/content/index.ts
+++ src/content/index.ts
@@ -1,4 +1,3 @@
-export { html as idea } from './idea.md';
 export { html as intro } from './intro.md';
 export { html as idea } from './idea.md';
 export { html as formation } from './formation.md';
