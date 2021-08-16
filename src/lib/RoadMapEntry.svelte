a45d3d2cc50d76cd7e5cfa0856fb4d887b56471f
Max Richter <max.richter@smail.th-koeln.de>
2021-08-16 12:53:41 +0000

    chore: remove unused css class

--- src/lib/RoadMapEntry.svelte
+++ src/lib/RoadMapEntry.svelte
@@ -13,78 +13,75 @@
 		showDesc = !showDesc;
 	}
 </script>
-<div class='entry'>
-    <div class='wrapper' on:click={switchDesc} class:opened={showDesc}>
-        <img srcset={StockImages[image]} alt='descriptive image of {name}' />
-        <div class='header'>
-            <h1 class='heading'>{name}</h1>
-            <p class='date'>{date}</p>
-        </div>
-    </div>
-        {#if showDesc}
-            <div transition:slide>
-                <p class='desc'>{description}</p>
-            </div>
-    {/if}
+
+<div class="entry">
+	<div class="wrapper" on:click={switchDesc} class:opened={showDesc}>
+		<img srcset={StockImages[image]} alt="descriptive image of {name}" />
+		<div class="header">
+			<h1 class="heading">{name}</h1>
+			<p class="date">{date}</p>
+		</div>
+	</div>
+	{#if showDesc}
+		<div transition:slide>
+			<p class="desc">{description}</p>
+		</div>
+	{/if}
 </div>
 
-<style lang='scss'>
-    @import '../styles/index.scss';
-    .entry {
-        border-radius: 1rem;
-        /* width: 50%; */
-        max-width: 768px;
-        background-color: #f8f8f8;
-        margin-bottom: 4rem;
-    }
-    .wrapper {
+<style lang="scss">
+	@import '../styles/index.scss';
+	.entry {
+		border-radius: 1rem;
+		/* width: 50%; */
+		max-width: 768px;
+		background-color: #f8f8f8;
+		margin-bottom: 4rem;
+	}
+	.wrapper {
 		cursor: pointer;
-        position: relative;
-        overflow: hidden;
-        height: 10rem;
-        border-radius: 13px;
-        transition: border-radius 0.5s;
-        background: $green;
-        &.opened {
-            border-bottom-left-radius: 0;
-            border-bottom-right-radius: 0;
-        }
-    }
-    img {
-        width: 100%;
-        filter: grayscale(1);
-        opacity: 0.5;
-        
-    }
-    .header {
-        position: absolute;
-        display: flex;
-        flex-direction: row;
-        width: 100%;
-        height: 100%;
-        justify-content: space-between;
-        top: 0;
-        left: 0;
-        background-color: rgba(0,0,0,0.3);
-    }
-    .heading {
-        margin: 1rem;
-        font-size: 1.5rem;
-        font-weight: normal;
-        color: white;
-    }
-    .date {
-        /* font-weight: 900; */
-        margin: 1rem;
-        font-size: 2.3rem;
-        color: rgba($color: #FFFFFF, $alpha: 0.6);
-    }
-    .desc {
-        padding: 2.3rem 1.7rem;
-    }
-    .gray {
-        background-color: $light-gray;
-    }
+		position: relative;
+		overflow: hidden;
+		height: 10rem;
+		border-radius: 13px;
+		transition: border-radius 0.5s;
+		background: $green;
+		&.opened {
+			border-bottom-left-radius: 0;
+			border-bottom-right-radius: 0;
+		}
+	}
+	img {
+		width: 100%;
+		filter: grayscale(1);
+		opacity: 0.5;
+	}
+	.header {
+		position: absolute;
+		display: flex;
+		flex-direction: row;
+		width: 100%;
+		height: 100%;
+		justify-content: space-between;
+		top: 0;
+		left: 0;
+		background-color: rgba(0, 0, 0, 0.3);
+	}
+	.heading {
+		margin: 1rem;
+		font-size: 1.5rem;
+		font-weight: normal;
+		color: white;
+	}
+	.date {
+		/* font-weight: 900; */
+		margin: 1rem;
+		font-size: 2.3rem;
+		color: rgba($color: #ffffff, $alpha: 0.6);
+	}
+	.desc {
+		padding: 2.3rem 1.7rem;
+	}
 
 	@media (max-width: 768px) {
 		.entry {
